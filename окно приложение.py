67e21bbc8b74bf38e22c915dff2ecb5622b9e462
osselina2303-lab <osselina.2303@gmail.com>
2026-05-17 17:05:07 +0700

    Update окно приложение.py

--- окно приложение.py
+++ окно приложение.py
@@ -8,43 +8,33 @@ class MainWindow(QMainWindow):
         self.initUI()
     
     def initUI(self):
-        # Настройка главного окна
-        self.setWindowTitle("Мое приложение на PyQt5")
+        self.setWindowTitle("Мое приложение")
         self.setMinimumSize(400, 300)
         
-        # Создание центрального виджета
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         
-        # Создание вертикального layout
         v_layout = QVBoxLayout()
         central_widget.setLayout(v_layout)
         
-        # Создание метки (QLabel)
         self.label = QLabel("Введите текст:")
         v_layout.addWidget(self.label)
         
-        # Создание текстового поля (QLineEdit)
         self.text_input = QLineEdit()
         v_layout.addWidget(self.text_input)
         
-        # Создание горизонтального layout для кнопок
         h_layout = QHBoxLayout()
         
-        # Создание кнопки 1
         self.button1 = QPushButton("Нажми меня")
         self.button1.clicked.connect(self.on_button_click)
         h_layout.addWidget(self.button1)
         
-        # Создание кнопки 2
         self.button2 = QPushButton("Очистить")
         self.button2.clicked.connect(self.clear_text)
         h_layout.addWidget(self.button2)
         
-        # Добавление горизонтального layout в вертикальный
         v_layout.addLayout(h_layout)
         
-        # Создание метки для результата
         self.result_label = QLabel("Результат будет здесь")
         v_layout.addWidget(self.result_label)
     
